5eb8cae99f80f4bdbcf8b21a478d11fd2305d3f2
Avoqato2 <mt241087@fhstp.ac.at>
2025-12-26 12:31:13 +0100

    Backend: Add *express *cors *db.ts

--- ai-server/src/index.ts
+++ ai-server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+import {loadEnvFile} from "node:process";
+loadEnvFile();
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
