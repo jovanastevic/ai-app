af5d309a11d83f0f14ee7bb5ca37af673eb2f6f1
Avoqato2 <mt241087@fhstp.ac.at>
2026-01-06 16:08:57 +0100

    * ws, body-parser added * added chat files

--- ai-server/src/index.ts
+++ ai-server/src/index.ts
@@ -2,13 +2,18 @@ import {loadEnvFile} from "node:process";
 loadEnvFile();
 import express from "express";
 import cors from "cors";
+import {createServer} from "node:http";
+import {WebSocketServer} from "ws";
 import {AuthController} from "./controller/AuthController";
 import {UserController} from "./controller/UserController";
 import {CategoryController} from "./controller/CategoryController";
 import {PromptController} from "./controller/PromptController";
+//import {ChatController} from "./controller/ChatController";
 
 
 const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({server});
 
 app.use(express.json());
 app.use(cors());
@@ -17,7 +22,8 @@ AuthController.init(app);
 UserController.init(app);
 CategoryController.init(app);
 PromptController.init(app);
+//ChatController.init(wss);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
