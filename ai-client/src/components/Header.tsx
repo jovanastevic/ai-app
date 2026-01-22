9c9215e891ba60e130a7972a4efef47efb88cbdf
Jovana Stevic <jovana.stevic31@gmail.com>
2026-01-22 22:41:12 +0100

    implemented dark mode

--- ai-client/src/components/Header.tsx
+++ ai-client/src/components/Header.tsx
@@ -1,19 +1,21 @@
 import {UserAvatar} from "./UserAvatar";
-import {Button} from "@/components/ui/button";
 import logo from "@/assets/logo-yea.svg";
+import logoLight from "@/assets/logo-yea-light.svg";
+import {ModeToggle} from "@/components/ModeToggle.tsx";
 
 export function Header() {
     return (
         <header className="w-full h-18 flex items-center border-b justify-between p-3">
             <div className="flex items-center">
-                <a href="#">
-                    <img src={logo.src} alt="Logo" width="40vw"/>
+                <a href="/#">
+                    <img src={logoLight.src} alt="Logo" className="h-8 w-auto hidden dark:block"/>
+                    <img src={logo.src} alt="Logo" className="h-8 w-auto block dark:hidden"/>
                 </a>
                 <h1 className="text-3xl font-bold">Actual Intelligence</h1>
             </div>
             <div className="flex items-center gap-4">
-                <Button>Dark</Button>
-                <UserAvatar/>
+                <ModeToggle />
+                <UserAvatar />
             </div>
         </header>
     )
