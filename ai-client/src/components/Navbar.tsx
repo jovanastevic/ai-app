39ab6d6e7b7807c35bd1bb627e2688d8f0d30503
Jovana Stevic <jovana.stevic31@gmail.com>
2026-01-22 10:23:19 +0100

    install shadcn & components

--- ai-client/src/components/Navbar.tsx
+++ ai-client/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import {useState} from "react";
+import { Button } from "@/components/ui/button"
 
 export default function Counter() {
     const [count, setCount] = useState(0);
     return (
-        <button onClick={() => setCount(count + 1)}>
+        <Button onClick={() => setCount(count + 1)}>
             count is {count}
-        </button>
+        </Button>
     )
 }
